55926e3053e1bd38368a6d3773f10f33c010a09a
Dell <Dell@purna-PC>
2017-09-17 00:21:25 +0530

    first change the program

--- CoreJavaProject/src/com/Simple.java
+++ CoreJavaProject/src/com/Simple.java
@@ -5,6 +5,7 @@ public class Simple {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("This is simple application");
+		System.out.println("modify-1,This is simple application");
 	}
 
 }
